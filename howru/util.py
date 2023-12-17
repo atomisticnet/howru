9bf02fcdec0f1b0d3238045c2676f27d2e0e9e42
Brian Donovan <bcd2136@columbia.edu>
2023-12-17 13:56:11 -0500

    Updated util.py with create_csv_from_entries and other functions to support this new function

--- howru/util.py
+++ howru/util.py
@@ -4,12 +4,12 @@ Collection of utility functions.
 """
 
 import itertools
-
+import pymatgen
 from pymatgen.apps.borg.hive import VaspToComputedEntryDrone
 from pymatgen.apps.borg.queen import BorgQueen
 
 
-__date__ = "2020-07-23"
+__date__ = "2023-12-17"
 __version__ = "0.1"
 
 
@@ -112,3 +112,106 @@ def load_entries_json(entryfiles, verbose=False):
             len(real_local_entries), entryfiles))
 
     return real_local_entries
+
+
+import csv
+import re
+from math import gcd
+from functools import reduce
+
+def parse_formula(formula):
+    """
+    Parse the chemical formula into elements and their counts.
+    
+    Arguments:
+
+    formula (str) chemical formula that will be parsed
+    """
+    elements = re.findall(r'([A-Z][a-z]*)(\d*)', formula)
+    return {elem: int(count) if count else 1 for elem, count in elements}
+
+def calculate_gcd(numbers):
+    """
+    Calculate the greatest common divisor of a list of numbers.
+    
+    Arguments:
+    numbers (list) input of numbers that will then return the GCD
+    """
+    return reduce(gcd, numbers)
+
+def empirical_formula(compound):
+    """
+    Calculate the empirical formula and the number of formula units.
+
+    Arguments:
+    compound (str): The chemical formula, e.g., 'Fe4S8'
+    """
+    elements_counts = parse_formula(compound)
+    counts = list(elements_counts.values())
+
+    # Find the greatest common divisor for the counts
+    gcd_counts = calculate_gcd(counts)
+
+    # Divide all counts by the gcd to get the simplest ratio
+    simplest_ratio = {element: count // gcd_counts for element, count in elements_counts.items()}
+
+    # Construct the empirical formula
+    empirical_formula = ''.join(f"{element}{(count if count > 1 else '')}" for element, count in simplest_ratio.items())
+
+    # Number of formula units is the sum of the counts in the empirical formula
+    num_units = sum(simplest_ratio.values())
+
+    return empirical_formula, num_units
+
+
+
+def create_csv_from_entries(entries, csv_file, exp_data_csv):
+    """
+    Create a CSV file from VASP calculation entries.
+    
+    Arguments:
+    entries (list): List of entries from load_entries_json function
+    csv_file (str): Path to save the CSV file
+    exp_data_csv (str): Path to the CSV file with experimental formation energies
+    """
+    # Read experimental formation energies into a dictionary
+    exp_energies = {}
+    with open(exp_data_csv, 'r') as file:
+        reader = csv.reader(file)
+        next(reader, None)  # Skip the header
+        for row in reader:
+            compound, energy = row[0], float(row[1])  # Assuming energy is a float
+            exp_energies[compound] = energy
+
+    headers = ['Compound Name', 'Empirical Formula', 'U Parameter (eV)', 'Total Energy From Vasp (eV)', 
+               'Energy Per Sulfur (eV)', 'Comp Formation Energy', 'Experimental Formation Energy (eV/atom)']
+
+    # Open the file in write mode
+    with open(csv_file, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(headers)
+
+        # Iterate over the entries and write the data rows
+        for entry in entries:
+            compound_name = entry.get_compound_name()  # Adjust this based on your entries structure
+            empirical_formula_str, num_units = empirical_formula(compound_name)
+            u_parameter = entry.get_u_parameter()  # Adjust this based on your entries structure
+            total_energy = entry.total_energy  # Adjust this based on your entries structure
+
+            # Check if num_units is zero to avoid division by zero
+            if num_units == 0:
+                continue
+
+            # Calculate the formation energy
+            formula_elements = parse_formula(empirical_formula_str)
+            total_element_energy = sum(elemental_energies.get(elem, 0) * count for elem, count in formula_elements.items())
+            energy_per_unit = total_energy / num_units
+            formation_energy = energy_per_unit - total_element_energy
+
+            exp_energy = exp_energies.get(compound_name, 'N/A')
+
+            # Write the row to the CSV file
+            writer.writerow([compound_name, empirical_formula_str, u_parameter, total_energy, energy_per_unit, formation_energy, exp_energy])
+
+    print(f"CSV file created at {csv_file}")
+
